866f3c6ef37afee85e234d5fd10edbcd8bf6c70c
Javier <javier.arredondo.c@usach.cl>
2021-03-11 11:50:21 -0300

    change in entrypoint (worker and threads)

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gunicorn -b $APP_BIND:$APP_PORT -w $APP_WORKERS "stamp_service.server:create_app({})"
+gunicorn -b $APP_BIND:$APP_PORT -w $APP_WORKERS --threads=$APP_THREADS "stamp_service.server:create_app({})"
