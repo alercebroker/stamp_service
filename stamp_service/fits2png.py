988088e727cbb326aab794d7b40bb3f7750fded8
Diego Rodriguez <diegorodriguezmancini@gmail.com>
2021-05-06 09:37:32 -0400

    log data to find error

--- stamp_service/fits2png.py
+++ stamp_service/fits2png.py
@@ -1,40 +1,44 @@
 import astropy.io.fits as fio
 import matplotlib.pyplot as plt
+from flask import current_app
 import io
 import gzip
 import numpy as np
+
 
 def get_max(compressed_fits_file, window):
     f = io.BytesIO(compressed_fits_file)
     gf = gzip.open(f)
     hdu = fio.open(gf)[0]
 
-    x = hdu.shape[0]//2
-    y = hdu.shape[1]//2
+    x = hdu.shape[0] // 2
+    y = hdu.shape[1] // 2
     n = window
     data = hdu.data
-    center = data[np.arange(x-n,x+n),:]
-    center = center[:,np.arange(y-n,y+n)]
+    center = data[np.arange(x - n, x + n), :]
+    center = center[:, np.arange(y - n, y + n)]
     max_val = np.max(center)
-    min_val = np.min(data) + 0.2*np.median(np.abs(data - np.median(data)))
+    min_val = np.min(data) + 0.2 * np.median(np.abs(data - np.median(data)))
 
-    return max_val,min_val
+    return max_val, min_val
 
-def transform(compressed_fits_file, file_type, max_val,min_val):
+
+def transform(compressed_fits_file, file_type, max_val, min_val):
     f = io.BytesIO(compressed_fits_file)
     gf = gzip.open(f)
     hdu = fio.open(gf)[0]
 
     data = hdu.data
+    current_app.logger.info(f"Transform data: {data}")
     if file_type != "difference":
         data[data > max_val] = max_val
         data[data < min_val] = min_val
 
     plt.figure()
-    plt.matshow(data,cmap='Greys_r',interpolation="nearest")
+    plt.matshow(data, cmap="Greys_r", interpolation="nearest")
     plt.axis("off")
     buf = io.BytesIO()
-    plt.savefig(buf,format="png", bbox_inches='tight', transparent=True)
+    plt.savefig(buf, format="png", bbox_inches="tight", transparent=True)
     buf.seek(0)
     im = buf.read()
     plt.cla()
