721ceb3037eba5885f68990134520cff989922bf
Pablo Castellanos <pablocastellanos@gmail.com>
2022-06-15 11:54:06 -0400

    Refactored utils and fits2png

--- stamp_service/fits2png.py
+++ stamp_service/fits2png.py
@@ -4,26 +4,28 @@ import io
 import gzip
 import numpy as np
 
-def get_max(compressed_fits_file, window):
-    f = io.BytesIO(compressed_fits_file)
-    gf = gzip.open(f)
-    hdu = fio.open(gf)[0]
 
-    x = hdu.shape[0]//2
-    y = hdu.shape[1]//2
-    n = window
+def _read_fits(compressed_fits_file):
+    fits = io.BytesIO(compressed_fits_file)
+    return fio.open(gzip.open(fits))[0]
+
+
+def get_max(compressed_fits_file, window):
+    hdu = _read_fits(compressed_fits_file)
+
+    x = hdu.shape[0] // 2
+    y = hdu.shape[1] // 2
     data = hdu.data
-    center = data[np.arange(x-n,x+n),:]
-    center = center[:,np.arange(y-n,y+n)]
+    center = data[np.arange(x - window, x + window), :]
+    center = center[:, np.arange(y - window, y + window)]
     max_val = np.max(center)
-    min_val = np.min(data) + 0.2*np.median(np.abs(data - np.median(data)))
+    min_val = np.min(data) + 0.2 * np.median(np.abs(data - np.median(data)))
 
     return max_val,min_val
 
+
 def transform(compressed_fits_file, file_type, max_val,min_val):
-    f = io.BytesIO(compressed_fits_file)
-    gf = gzip.open(f)
-    hdu = fio.open(gf)[0]
+    hdu = _read_fits(compressed_fits_file)
 
     data = hdu.data
     if file_type != "difference":
@@ -31,10 +33,10 @@ def transform(compressed_fits_file, file_type, max_val,min_val):
         data[data < min_val] = min_val
 
     plt.figure()
-    plt.matshow(data,cmap='Greys_r',interpolation="nearest")
+    plt.matshow(data, cmap='Greys_r', interpolation="nearest")
     plt.axis("off")
     buf = io.BytesIO()
-    plt.savefig(buf,format="png", bbox_inches='tight', transparent=True)
+    plt.savefig(buf, format="png", bbox_inches='tight', transparent=True)
     buf.seek(0)
     im = buf.read()
     plt.cla()
